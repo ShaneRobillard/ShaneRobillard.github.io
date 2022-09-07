a5596e1d8426f01d39bf5a1dc4804f5a68f6cfb7
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2022-09-07 18:59:30 -0400

    Merge

---
+++ itsc-3155/app.py
@@ -0,0 +1,15 @@
+itemlist = []
+number_items = int(input("Number of Items: "))
+
+for x in range(0,number_items):
+    item_type = input("Input item and price: ")
+    itemlist.append(item_type)
+    
+for i in range(number_items -1):
+    for j in range(number_items - i - 1):
+        if(itemlist[j][1] > itemlist[j+1][1]):
+            temp = itemlist[j]
+            itemlist[j] = itemlist[j+1]
+            itemlist[j+1] = temp
+
+print(itemlist)
