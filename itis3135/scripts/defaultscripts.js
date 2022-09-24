999735076882154bac5aa27f0f7de704dcd2aaf0
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2022-09-23 20:55:02 -0400

    webpage update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -15,12 +15,32 @@ function server(){
     var servers = document.getElementById("servers").value;
     var months = document.getElementById("months").value;
     var price = servers * months * 35;    
-    document.getElementById("price").innerHTML = "Your "+servers+ " servers will cost you $" +price+ "!";
+    document.getElementById("price").innerHTML = "Your "+servers+ " servers will cost you: $" +price+ "!";
 }
 
-function product(){
-    var first = document.getElementById("first").value;
-    var second = document.getElementById("second").value;
-    var multiply = first * second;
-    document.getElementById("product").innerHTML = "The product of the two numbers you entered is: " +multiply+ ".";
+function bandwidth(){
+    var bandwidth = document.getElementById("bandwidthcost").value;
+    var costs = bandwidth * 60;
+    document.getElementById("costs").innerHTML = "The total price for requested bandwidth is $" +costs+ " a month!";
+}
+
+function frontServices(){
+    var firstfront = document.getElementById("firstfront").value;
+    var secondfront = document.getElementById("secondfront").value;
+    var services = firstfront * secondfront * 175;
+    document.getElementById("frontservices").innerHTML = "The price of the service is: $" +services+ ".";
+}
+
+function backServices(){
+    var firstback = document.getElementById("firstback").value;
+    var secondback = document.getElementById("secondback").value;
+    var services = firstback * secondback * 215;
+    document.getElementById("backservices").innerHTML = "The price of the service is: $" +services+ ".";
+}
+
+function webServices(){
+    var firstweb = document.getElementById("firstweb").value;
+    var secondweb = document.getElementById("secondweb").value;
+    var services = firstweb * secondweb * 150;
+    document.getElementById("webservices").innerHTML = "The price of the service is: $" +services+ ".";
 }
