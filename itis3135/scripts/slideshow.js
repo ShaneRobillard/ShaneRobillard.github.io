540353630d1df9fc94a8b0f496145c802d418f7f
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2022-11-29 09:08:14 -0500

    website update

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,26 +1,32 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slideshow = ["#amaryllis","#closeup","#flowers","#herbs","#hibiscus","#hibiscus2"]
+var i=0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+$(document).ready(function () {
+  $('.pictures').hide();
+  $(slideshow[0]).show();
+});
+
+function next(){
+  i++;
+  if(i==6){
+    //$('#hibiscus2').hide(slideshow[5]);
+    //$('#hibiscus2').show(slideshow[0]);
+    i=0;
+  }
+  if(i==5){
+    //$('#hibiscus2').show(slideshow[5]);
+  }
+  $(slideshow[i-1]).hide();
+  $(slideshow[i]).show();
+  console.log(i);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function prev(){
+  $(slideshow[i-1]).show();
+  $(slideshow[i]).hide();
+  if(i<1){
+    i=5;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  i-=1;
+  console.log(i);
 }
