e70097faf81f8fd4da3d49ff05528eac51c3d0d1
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2022-12-07 10:31:57 -0500

    website update

--- itis3135/Activity13/robillard-Activity13.js
+++ itis3135/Activity13/robillard-Activity13.js
@@ -1,28 +1,3 @@
-// $(document).ready(function() {
-//     $.ajax({
-//         type: "get",
-//         url: "team.json",
-//         beforeSend: function() {
-//             $("#team").html("Loading...");
-//         },
-//         timeout: 10000,
-//         error: function(xhr, status, error) {
-//             alert("Error: " + xhr.status + " - " + error);
-//         },
-//         dataType: "json",
-//         success: function(data) {
-//             $("#team").html("");
-//             $(data).find("management").children().each(function() {
-//                 var xmlDoc = $(this);
-//                 $("#team").append
-//                 ("<h3>" + xmlDoc.find("name").text() + "</h3>" +
-//                           xmlDoc.find("title").text() + "<br>" +
-//                           xmlDoc.find("bio").text() + "<br>");
-//             });
-//         }
-//     });
-// });
-
 $(document).ready(function(){ 
     $.getJSON("team.json", function(par){ 
         $.each(par, function() { 
