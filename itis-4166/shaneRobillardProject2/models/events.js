4dc6128d83ac8f3e52843cf0c1bd7b0d4b848e0b
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2023-02-23 13:40:30 -0500

    website update

--- itis-4166/shaneRobillardProject2/models/events.js
+++ itis-4166/shaneRobillardProject2/models/events.js
@@ -7,42 +7,63 @@ const events = [
     hostName: 'Blue Wolf',
     startTime: 'April 22nd, 2023, 6pm',
     endTime: 'April 22nd, 2023, 11pm',
+    location: 'Blue Wolf Observatory',
     details: 'Come and join us in watching the Lyrids Meteor Showers.',
-    image: ''
+    image: img.src='../../public/images/meteorshower.jpg'
 },
 {
-    id:'2',
-    title: 'Learning NBAD',
-    content: 'NBAD is an acronym. It stands for something.',
-    author: 'Shane Robillard',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    id: '2',
+    category: 'Viewing Parties',
+    title: 'Meeting at the Observatory',
+    hostName: 'Blue Wolf',
+    startTime: 'April 21st, 2023, 6pm',
+    endTime: 'April 21st, 2023, 11pm',
+    location: 'Blue Wolf Observatory',
+    details: 'Join us as we use the Observatory to peer into the heavens.',
+    image: img.src='../../public/images/observatory.jpg'
 },
 {
-    id:'3',
-    title: 'My Spring Break',
-    content: 'My spring break is going to be spent with my kids. I dont plan on going anywhere at this time.',
-    author: 'Shane Robillard',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    id: '3',
+    category: 'Viewing Parties',
+    title: 'Star Parties for Beginners',
+    hostName: 'Blue Wolf',
+    startTime: 'April 20th, 2023, 6pm',
+    endTime: 'April 20th, 2023, 11pm',
+    location: 'Blue Wolf Observatory',
+    details: 'This party is for newcomers to socialize and learn from our more experienced members. ',
+    image: img.src='../../public/images/starparty.jpg'
 },
 {
     id: '4',
-    title: 'My life at Charlotte',
-    content: 'My life at Charlotte has been a short one. I love the university and what it has to teach me. I have tried learning to the best of my ability.',
-    author: 'Shane Robillard',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    category: 'Educational Classes',
+    title: 'The Science of Space Telescopes',
+    hostName: 'Blue Wolf',
+    startTime: 'April 22nd, 2023, 3pm',
+    endTime: 'April 22nd, 2023, 5pm',
+    location: 'Blue Wolf Classroom',
+    details: 'This class is for beginners to learn about how telescopes work.',
+    image: img.src='../../public/images/telescope.jpg'
 },
 {
-    id:'5',
-    title: 'Learning NBAD',
-    content: 'NBAD is an acronym. It stands for something.',
-    author: 'Shane Robillard',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    id: '5',
+    category: 'Educational Classes',
+    title: 'Buying A Telescope',
+    hostName: 'Blue Wolf',
+    startTime: 'April 21st, 2023, 3pm',
+    endTime: 'April 21st, 2023, 5pm',
+    location: 'Blue Wolf Classroom',
+    details: 'This class is designed to educate about what telescopes are out there to buy.',
+    image: img.src='../../public/images/buyingtelescope.jpg'
 },
 {
-    id:'6',
-    title: 'My Spring Break',
-    content: 'My spring break is going to be spent with my kids. I dont plan on going anywhere at this time.',
-    author: 'Shane Robillard',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
-}
+    id: '6',
+    category: 'Educational Classes',
+    title: 'Reading a Star Atlas',
+    hostName: 'Blue Wolf',
+    startTime: 'April 20th, 2023, 3pm',
+    endTime: 'April 20th, 2023, 5pm',
+    location: 'Blue Wolf Classroom',
+    details: 'This class is for those who wish to learn about the different types of star atlases, and how to read them.',
+    image: img.src='../../public/images/staratlas.jpg'
+},
 ];
