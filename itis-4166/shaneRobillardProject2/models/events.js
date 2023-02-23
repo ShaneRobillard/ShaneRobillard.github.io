df725c72a353588959fef712856125442256dab7
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2023-02-23 13:18:51 -0500

    website update

---
+++ itis-4166/shaneRobillardProject2/models/events.js
@@ -0,0 +1,48 @@
+const { v4: uuidv4 } = require('uuid');
+const events = [
+{
+    id: '1',
+    category: 'Viewing Parties',
+    title: 'Meteor Showers',
+    hostName: 'Blue Wolf',
+    startTime: 'April 22nd, 2023, 6pm',
+    endTime: 'April 22nd, 2023, 11pm',
+    details: 'Come and join us in watching the Lyrids Meteor Showers.',
+    image: ''
+},
+{
+    id:'2',
+    title: 'Learning NBAD',
+    content: 'NBAD is an acronym. It stands for something.',
+    author: 'Shane Robillard',
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+},
+{
+    id:'3',
+    title: 'My Spring Break',
+    content: 'My spring break is going to be spent with my kids. I dont plan on going anywhere at this time.',
+    author: 'Shane Robillard',
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+},
+{
+    id: '4',
+    title: 'My life at Charlotte',
+    content: 'My life at Charlotte has been a short one. I love the university and what it has to teach me. I have tried learning to the best of my ability.',
+    author: 'Shane Robillard',
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+},
+{
+    id:'5',
+    title: 'Learning NBAD',
+    content: 'NBAD is an acronym. It stands for something.',
+    author: 'Shane Robillard',
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+},
+{
+    id:'6',
+    title: 'My Spring Break',
+    content: 'My spring break is going to be spent with my kids. I dont plan on going anywhere at this time.',
+    author: 'Shane Robillard',
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+}
+];
