2d0c1617039f5e76103ae40ad974fa1af1ba5418
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2023-02-25 14:38:59 -0500

    website update

--- itis-4166/shaneRobillardProject2/models/events.js
+++ itis-4166/shaneRobillardProject2/models/events.js
@@ -6,8 +6,8 @@ const events = [
     category: 'Viewing Parties',
     title: 'Meteor Showers',
     hostName: 'Blue Wolf',
-    startTime: 'April 22nd, 2023, 6pm',
-    endTime: 'April 22nd, 2023, 11pm',
+    startTime: DateTime.local(2023, 4, 22, 18).toLocaleString(DateTime.DATETIME_MED),
+    endTime: DateTime.local(2023, 4, 22, 23).toLocaleString(DateTime.DATETIME_MED),
     location: 'Blue Wolf Observatory',
     details: 'Come and join us in watching the Lyrids Meteor Showers.',
     image: '/images/meteorshower.jpg'
@@ -17,8 +17,8 @@ const events = [
     category: 'Viewing Parties',
     title: 'Meeting at the Observatory',
     hostName: 'Blue Wolf',
-    startTime: 'April 21st, 2023, 6pm',
-    endTime: 'April 21st, 2023, 11pm',
+    startTime: DateTime.local(2023, 4, 21, 18).toLocaleString(DateTime.DATETIME_MED),
+    endTime: DateTime.local(2023, 4, 21, 23).toLocaleString(DateTime.DATETIME_MED),
     location: 'Blue Wolf Observatory',
     details: 'Join us as we use the Observatory to peer into the heavens.',
     image: '/images/observatory.jpg'
@@ -28,8 +28,8 @@ const events = [
     category: 'Viewing Parties',
     title: 'Star Parties for Beginners',
     hostName: 'Blue Wolf',
-    startTime: 'April 20th, 2023, 6pm',
-    endTime: 'April 20th, 2023, 11pm',
+    startTime: DateTime.local(2023, 4, 20, 18).toLocaleString(DateTime.DATETIME_MED),
+    endTime: DateTime.local(2023, 4, 20, 23).toLocaleString(DateTime.DATETIME_MED),
     location: 'Blue Wolf Observatory',
     details: 'This party is for newcomers to socialize and learn from our more experienced members. ',
     image: '/images/starparty.jpg'
@@ -39,8 +39,8 @@ const events = [
     category: 'Educational Classes',
     title: 'The Science of Space Telescopes',
     hostName: 'Blue Wolf',
-    startTime: 'April 22nd, 2023, 3pm',
-    endTime: 'April 22nd, 2023, 5pm',
+    startTime: DateTime.local(2023, 4, 22, 15).toLocaleString(DateTime.DATETIME_MED),
+    endTime: DateTime.local(2023, 4, 22, 17).toLocaleString(DateTime.DATETIME_MED),
     location: 'Blue Wolf Classroom',
     details: 'This class is for beginners to learn about how telescopes work.',
     image: '/images/telescope.jpg'
@@ -50,8 +50,8 @@ const events = [
     category: 'Educational Classes',
     title: 'Buying A Telescope',
     hostName: 'Blue Wolf',
-    startTime: 'April 21st, 2023, 3pm',
-    endTime: 'April 21st, 2023, 5pm',
+    startTime: DateTime.local(2023, 4, 21, 15).toLocaleString(DateTime.DATETIME_MED),
+    endTime: DateTime.local(2023, 4, 22, 17).toLocaleString(DateTime.DATETIME_MED),
     location: 'Blue Wolf Classroom',
     details: 'This class is designed to educate about what telescopes are out there to buy.',
     image: '/images/buyingtelescope.jpg'
@@ -61,8 +61,8 @@ const events = [
     category: 'Educational Classes',
     title: 'Reading a Star Atlas',
     hostName: 'Blue Wolf',
-    startTime: 'April 20th, 2023, 3pm',
-    endTime: 'April 20th, 2023, 5pm',
+    startTime: DateTime.local(2023, 4, 20, 15).toLocaleString(DateTime.DATETIME_MED),
+    endTime: DateTime.local(2023, 4, 20, 17).toLocaleString(DateTime.DATETIME_MED),
     location: 'Blue Wolf Classroom',
     details: 'This class is for those who wish to learn about the different types of star atlases, and how to read them.',
     image: '/images/staratlas.jpg'
@@ -79,6 +79,8 @@ exports.findById = function(id) {
 
 exports.save = function(event){
     event.id = uuidv4();
+    event.startTime = DateTime.fromISO(event.startTime).toLocaleString(DateTime.DATETIME_MED);
+    event.endTime = DateTime.fromISO(event.endTime).toLocaleString(DateTime.DATETIME_MED);
     events.push(event);
 };
 
@@ -87,8 +89,8 @@ exports.updateById = function(id, newEvent){
     if(event){
         event.title = newEvent.title;
         event.hostName = newEvent.hostName;
-        event.startTime = newEvent.startTime;
-        event.endTime = newEvent.endTime;
+        event.startTime = DateTime.fromISO(event.startTime).toLocaleString(DateTime.DATETIME_MED);
+        event.endTime = DateTime.fromISO(event.endTime).toLocaleString(DateTime.DATETIME_MED);
         event.location = newEvent.location;
         event.details = newEvent.details;
         event.image = newEvent.image;
