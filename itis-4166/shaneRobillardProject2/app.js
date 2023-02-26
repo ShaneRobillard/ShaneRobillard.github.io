2b9361b0020d083213a8c0d8f233468d675d9747
ShaneRobillard <111817640+ShaneRobillard@users.noreply.github.com>
2023-02-25 19:14:32 -0500

    website update

--- itis-4166/shaneRobillardProject2/app.js
+++ itis-4166/shaneRobillardProject2/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const mainRoute = require('./routes/mainRoute');
 const eventRoute = require('./routes/eventRoute');
-
 const {fileUpload} = require('./middleware/fileUpload');
 
 //create application
